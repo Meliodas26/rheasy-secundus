d4b0b17a5e4ffaa88fd05827f98691d92520fd85
Aldogtz26 <60399727+Aldogtz26@users.noreply.github.com>
2021-03-24 23:17:17 -0600

    [FIX] Footer.php and Header.php

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-	<html class="margin-top-default" lang="ES" <?php language_attributes(); ?>
- 		<head>
-		 	<title><?php bloginfo('name'); ?></title>
-   			<meta charset="<?php bloginfo('charset'); ?>">
-   			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-   			<?php wp_head(); ?>
-		</head>
-		<body>
+<html class="margin-top-default" lang="ES" <?php language_attributes(); ?>
+	<head>
+		<title><?php bloginfo('name'); ?></title>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<?php wp_head(); ?>
+	</head>
+	<body>
